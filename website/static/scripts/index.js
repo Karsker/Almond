45243da4f70b1f2a5417f5fad4749122432edb60
‘Karthik <karthikchandrasekhar03@outlook.com>
2023-12-18 16:55:39 +0530

    Added navbar

---
+++ website/static/scripts/index.js
@@ -0,0 +1,13 @@
+function showResourcesFlyover() {
+    let flyover = document.querySelector("#flyoverContainer");
+    flyover.style.display = 'block';
+    
+    
+}
+
+function hideResourcesFlyover() {
+    let flyover = document.querySelector("#flyoverContainer");
+    flyover.style.display = 'none';
+    
+    
+}
